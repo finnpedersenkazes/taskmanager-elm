63279817d7a0515ab71b93d5d2b987d4228d8f05
Finn Pedersen <finnpedersenfrance@gmail.com>
2023-09-29 10:10:11 +0200

    Updates

---
+++ precache-manifest.6be5fcd20d0f7511d342ea41c72fc9b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/taskmanager-elm/favicon.ico"
+  },
+  {
+    "revision": "f35d51b3153ba0a2b4fa68caa7357496",
+    "url": "/taskmanager-elm/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/taskmanager-elm/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/taskmanager-elm/manifest.json"
+  },
+  {
+    "revision": "1b238f5fdebd8fc58607",
+    "url": "/taskmanager-elm/static/css/main.6c3d579e.chunk.css"
+  },
+  {
+    "revision": "1b238f5fdebd8fc58607",
+    "url": "/taskmanager-elm/static/js/main.1b238f5f.chunk.js"
+  },
+  {
+    "revision": "8e20190d8efb772864b9",
+    "url": "/taskmanager-elm/static/js/runtime~main.8e20190d.js"
+  },
+  {
+    "revision": "ded87515c392c3502f6c",
+    "url": "/taskmanager-elm/static/js/vendors~main.ded87515.chunk.js"
+  }
+]);
